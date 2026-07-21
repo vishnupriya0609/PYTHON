7f67a3f477d39b3de1065c823e94fb8f28424da5
vishnupriya0609 <vishnupriya.t.cse26@psvpec.in>
2026-07-21 20:00:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+a=10
+b=15
+print(a+b)
+
+a=int(input("enter a: "))
+b=int(input("enter b: "))
+area=a*a
+print(area)
+
+a=10
+a=type(a)
+print(a)
+
+a=10
+name=str(a)
+print(name)
+print(type(name))
+
+radius=float(input("enter the radius: "))
+c=2*3.14*radius
+print(c)
+
+import math
+radius=float(input("enter the radius: "))
+c=2*math.pi*radius
+print(c)
+
